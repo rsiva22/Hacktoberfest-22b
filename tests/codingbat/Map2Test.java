40949120dcbb46e67d0059fd44e646eeebef3435
April Rieff <april.rieff@gmail.com>
2022-10-10 16:29:46 -0500

    map2 test

--- tests/codingbat/Map2Test.java
+++ tests/codingbat/Map2Test.java
@@ -1,9 +1,9 @@
 package codingbat;
 
-import codingbat.Map2;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,6 +15,14 @@ class Map2Test {
         String[] result = Map2.allSwap(strings);
         String[] expected = {"caterpillar", "dog", "butterfly", "cat", "kitten", "kangaroo", "bunny", "centipede", "crocodile"};
         assertEquals(Arrays.toString(expected), Arrays.toString(result));
+    }
+
+    @Test
+    void april_wordCount(){
+        String[] words = {"apple", "bee", "caramel", "dark", "bee", "elephant", "apple", "dark", "fog"};
+        Map<String, Integer> result = Map2.wordCount(words);
+        String[] expected = {"apple=2", "bee=2", "caramel=1", "dark=2", "elephant=1", "fog=1"};
+        assertEquals(Arrays.toString(expected), result);
     }
 
 }
