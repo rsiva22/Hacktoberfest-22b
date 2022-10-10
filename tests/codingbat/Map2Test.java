37351a2420a9bc83fce706a850306b2144badc4f
Andrew Cromwell <andrew.b.cromwell@gmail.com>
2022-10-10 16:30:57 -0500

    completed 2nd contribution

--- tests/codingbat/Map2Test.java
+++ tests/codingbat/Map2Test.java
@@ -17,4 +17,18 @@ class Map2Test {
         assertEquals(Arrays.toString(expected), Arrays.toString(result));
     }
 
+    @Test
+    void andy_firstSwap() {
+        // if no strings match
+        String[] strings = {"cat", "dog", "bunny", "horse"};
+        String[] result = Map2.firstSwap(strings);
+        String[] expected = {"cat", "dog", "bunny", "horse"};
+        assertEquals(Arrays.toString(expected), Arrays.toString(result));
+
+        // if strings match only swap first two that start with a given letter
+        String[] strings2 = {"cat", "hog", "cow", "caterpillar", "hamster", "coon", "horse", "Human"};
+        String[] result2 = Map2.firstSwap(strings2);
+        String[] expected2 = {"cow", "hamster", "cat", "caterpillar", "hog", "coon", "horse", "Human"};
+        assertEquals(Arrays.toString(expected2), Arrays.toString(result2));
+    } // andy_firstSwap end
 }
