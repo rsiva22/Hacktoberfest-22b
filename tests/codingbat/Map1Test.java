c029534ba452bbf956ce2c72d619318d00500eea
Ethan Reed McElree <ethan.mcelree@gmail.com>
2022-10-11 18:02:11 -0500

    Doing contribution 2

--- tests/codingbat/Map1Test.java
+++ tests/codingbat/Map1Test.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.HashMap;
 import java.util.Map;
 
+import static codingbat.Map1.topping1;
 import static org.junit.jupiter.api.Assertions.*;
 
 class Map1Test {
@@ -25,6 +26,34 @@ class Map1Test {
         Map<String, String> map4 = Map1.mapAB2(map3);
         assertTrue(map4.containsKey("a"));
         assertTrue(map4.containsKey("b"));
+
+    }
+
+    @Test
+    void ethanm_topping1() {
+        Map<String, String> map1 = new HashMap<>();
+        Map<String, String> map2 = new HashMap<>();
+        map1.put("ice cream", "peanuts");
+        map2.put("bread", "butter");
+        map2.put("ice cream", "cherry");
+        assertEquals(topping1(map1) , map2);
+
+        Map<String, String> map3 = new HashMap<>();
+        Map<String, String> map4 = new HashMap<>();
+        map4.put("bread", "butter");
+        assertEquals(topping1(map3) , map4);
+
+        Map<String, String> map5 = new HashMap<>();
+        Map<String, String> map6 = new HashMap<>();
+        map6.put("bread", "");
+        assertFalse(topping1(map5).equals(map6));
+
+        Map<String, String> map7 = new HashMap<>();
+        Map<String, String> map8 = new HashMap<>();
+        map7.put("pancake", "syrup");
+        map8.put("bread", "butter");
+        map8.put("pancake", "syrup");
+        assertTrue(topping1(map7).equals(map8));
 
     }
 
--- tests/codingbat/Map2Test.java
+++ tests/codingbat/Map2Test.java
@@ -4,7 +4,10 @@ import codingbat.Map2;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
+import static codingbat.Map2.wordLen;
 import static org.junit.jupiter.api.Assertions.*;
 
 class Map2Test {
@@ -15,6 +18,27 @@ class Map2Test {
         String[] result = Map2.allSwap(strings);
         String[] expected = {"caterpillar", "dog", "butterfly", "cat", "kitten", "kangaroo", "bunny", "centipede", "crocodile"};
         assertEquals(Arrays.toString(expected), Arrays.toString(result));
+    }
+    @Test
+    void ethanm_wordLen(){
+        Map<String, Integer> result1 = new HashMap<>();
+        result1.put("bb", 2);
+        result1.put("a", 1);
+        String[] stringArray1 = {"a", "bb", "a", "bb"};
+        assertEquals(Map2.wordLen(stringArray1), result1);
+
+        Map<String, Integer> result2 = new HashMap<>();
+        result2.put("this", 4);
+        result2.put("and", 3);
+        result2.put("that", 4);
+        String[] stringArray2 = {"this", "and", "that", "and"};
+        assertTrue(Map2.wordLen(stringArray2).equals(result2));
+
+        Map<String, Integer> result3 = new HashMap<>();
+        result3.put("this", 4);
+        result3.put("and", 3);
+        String[] stringArray3 = {"this", "and", "that", "and"};
+        assertFalse(Map2.wordLen(stringArray3).equals(result3));
     }
 
 }
