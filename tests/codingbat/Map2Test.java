36aca776533a69a02ba5af11ece83c547fe95fd6
Stephen Jaurigue <k0886570@student.kirkwood.edu>
2022-10-15 13:57:13 -0500

    Created wordMultiple Tests

--- tests/codingbat/Map2Test.java
+++ tests/codingbat/Map2Test.java
@@ -4,6 +4,7 @@ import codingbat.Map2;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,6 +16,25 @@ class Map2Test {
         String[] result = Map2.allSwap(strings);
         String[] expected = {"caterpillar", "dog", "butterfly", "cat", "kitten", "kangaroo", "bunny", "centipede", "crocodile"};
         assertEquals(Arrays.toString(expected), Arrays.toString(result));
+    }
+
+    @Test
+    void stephen_wordMultiple() {
+        String[] words = new String[] {"Hi", "Hello", "Hola", "Hi"};
+
+        Map<String,Boolean> moreThanOne = Map2.wordMultiple(words);
+
+        assertTrue(moreThanOne.get("Hi"));
+        assertFalse(moreThanOne.get("Hola"));
+
+
+        String[] words2 = new String[] {"Hi", "Hello", "Hola","Howdy","Hola", "Hi"};
+
+        Map<String,Boolean> moreThanOne2 = Map2.wordMultiple(words2);
+
+        assertTrue(moreThanOne2.get("Hi"));
+        assertTrue(moreThanOne2.get("Hola"));
+        assertFalse(moreThanOne2.get("Howdy"));
     }
 
 }
