1f2e2e70e8d9a1bb6107484a09497942e74fdfa5
John Morris <roxenthndr@gmail.com>
2022-10-17 12:21:00 -0500

    Commitment 2

--- tests/codingbat/Map2Test.java
+++ tests/codingbat/Map2Test.java
@@ -19,33 +19,6 @@ class Map2Test {
         assertEquals(Arrays.toString(expected), Arrays.toString(result));
     }
 
-//    /*
-//     * We'll say that 2 strings "match" if they are non-empty and their first
-//     * chars are the same. Loop over and then return the given array of
-//     * non-empty strings as follows: if a string matches an earlier string
-//     * in the array, swap the 2 strings in the array. A particular first char
-//     * can only cause 1 swap, so once a char has caused a swap, its later swaps
-//     * are disabled. Using a map, this can be solved making just one pass over
-//     * the array. More difficult than it looks.
-//     */
-//    public static String[] firstSwap(String[] strings) {
-//        Map<String, Integer> map = new HashMap<>();
-//        for(int i = 0; i < strings.length; i++) {
-//            String firstCharacter = strings[i].substring(0, 1);
-//            if(map.containsKey(firstCharacter)) {
-//                if(map.get(firstCharacter) != -1) {
-//                    String temp = strings[i];
-//                    strings[i] = strings[map.get(firstCharacter)];
-//                    strings[map.get(firstCharacter)] = temp;
-//                    map.put(firstCharacter, -1);
-//                }
-//            } else {
-//                map.put(firstCharacter, i);
-//            }
-//        }
-//        return strings;
-//    }
-
     @Test
     void john_firstSwap(){
         String[] str = {"goat","bat","john","bart","george", "kaitlyn","box"};
@@ -57,5 +30,6 @@ class Map2Test {
         assertTrue(result[6].equals("box"));
 
     }
+
 
 }
