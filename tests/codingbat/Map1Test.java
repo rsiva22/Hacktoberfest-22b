44af67e790f9100043fd39821b0af9b3611a0fbd
Gideon Lee Trevor <gideon-trevor@student.kirkwood.edu>
2022-10-17 11:50:05 -0500

    Contribution 2 - Gideon

--- tests/codingbat/Map1Test.java
+++ tests/codingbat/Map1Test.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.HashMap;
 import java.util.Map;
 
+import static codingbat.Map1.mapBully;
 import static org.junit.jupiter.api.Assertions.*;
 
 class Map1Test {
@@ -25,6 +26,22 @@ class Map1Test {
         Map<String, String> map4 = Map1.mapAB2(map3);
         assertTrue(map4.containsKey("a"));
         assertTrue(map4.containsKey("b"));
+
+    }
+
+    @Test
+    void gideon_mapBully() {
+        Map<String, String> map1 = new HashMap<>();
+        map1.put("a", "candy");
+        map1=mapBully(map1);
+        assertTrue(map1.containsKey("a"));
+        assertTrue(map1.containsKey("b"));
+        assertEquals("",map1.get("a"));
+        assertEquals("candy",map1.get("b"));
+        map1.put("a", "screamFest");
+        map1=mapBully(map1);
+        assertTrue(map1.containsKey("b"));
+        assertFalse("candy",map1.get("b"));
 
     }
 
--- tests/codingbat/Map2Test.java
+++ tests/codingbat/Map2Test.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
+import static codingbat.Map1.mapBully;
+import static codingbat.Map2.wordCount;
 import static org.junit.jupiter.api.Assertions.*;
 
 class Map2Test {
@@ -15,6 +17,20 @@ class Map2Test {
         String[] result = Map2.allSwap(strings);
         String[] expected = {"caterpillar", "dog", "butterfly", "cat", "kitten", "kangaroo", "bunny", "centipede", "crocodile"};
         assertEquals(Arrays.toString(expected), Arrays.toString(result));
+    }
+
+    @Test
+    void gideon_wordCount() {
+        Map<String, Integer> map = new HashMap<>();
+        String[] strings = {"a", "b", "a", "c", "b"};
+        map=wordCount(strings);
+        assertTrue(map.containsKey("a"));
+        assertTrue(map.containsKey("b"));
+        assertTrue(map.containsKey("c"));
+        assertEquals(2,map.get("a"));
+        assertEquals(2,map.get("b"));
+        assertEquals(1,map.get("c"));
+        assertFalse(2,map.get("c"));
     }
 
 }
